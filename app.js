f62d5fc5b73c2fb901893b0eba64d75246b93398
Yusuke_Kurihara <kurihara@infocity.co.jp>
2022-11-09 12:32:26 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 'use strict';
-function fib(n) {// 0 の時は 0 を返す処理
-    if (n === 0) {
-      return 0;
 
-    } else if (n === 1) {// 1 の時は 1 を返す処理
-        return 1;
+/*このコードで memo という名前の Map を作っています。キーには順番を、値には答えを格納します。
+ここで変数名を memo としているのは、 このように一度計算した結果を保存しておく方法を「メモ化」というためです。*/
+const memo = new Map();
+
+memo.set(0, 0);// 0 の時は 0 を返す処理
+memo.set(1, 1);// 1 の時は 1 を返す処理
+
+function fib(n) {
+    if (memo.has(n)) {
+      return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);// それ以外の時は、前と、前の前のフィボナッチ数を足し合わせた数を返す
+    const value = fib(n - 1) + fib(n - 2);// 前と、前の前のフィボナッチ数を足し合わせた数を返す
+    memo.set(n, value);
+    return value;
 }
 
 // 40 番目まで出力
 const length = 40;
 for (let i = 0; i <= length; i++) {
-  console.log(fib(i));
+    console.log(fib(i));
 }
